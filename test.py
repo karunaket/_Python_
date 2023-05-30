9617cd697ef6d79e9c5611a4f0ad718548600721
karuna ketan <81628692+karunaket@users.noreply.github.com>
2023-05-30 05:35:58 +0000

    dictionary

--- test.py
+++ test.py
@@ -5,3 +5,8 @@ print(type(var))'''
 
 message = 'Python is a fun programming language'
 print(message.find('fun'))
+
+
+something_3 = "Heyy Dude whats upp"
+print(something_3.count("u"))
+print(something_3.index("u"))
